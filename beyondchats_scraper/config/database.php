0bcb80c605967b1723c6e6f7645c0787a9bf2889
yuganter98 <yugantersharma99@gmail.com>
2025-12-26 20:41:11 +0530

    Cleanup: Revert IPv4 hack, using Supabase Pooler instead

--- beyondchats_scraper/config/database.php
+++ beyondchats_scraper/config/database.php
@@ -86,7 +86,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST') ? gethostbyname(env('DB_HOST')) : '127.0.0.1',
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
